63be959c05513472649e95d1f3a0dfdf92d0a943
benj <benjaminjaffe8@gmail.com>
2024-08-18 13:30:53 -0400

    Updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 import conversationRoutes from './routes/conversation.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import { app, server } from './socket/socket.js';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +21,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/conversations', conversationRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,23 @@
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+export default defineConfig(() => {
+  dotenv.config({ path: path.resolve(__dirname, '../.env') });
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': process.env,
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+        },
       },
     },
-  },
+  };
 });
